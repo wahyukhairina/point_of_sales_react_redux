a461e3b842ad4488f18a17afd4f035076f64ee26
ayukhairina <wahyukhairina@gmail.com>
2020-03-03 17:07:25 +0800

    add and reduce cart

--- src/components/product/CartItem.js
+++ src/components/product/CartItem.js
@@ -1,29 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const CartItem = ({ cart }) => {
-  return (
-    <>
-      <div className='row'>
-        <div className='col-md-4'>
-          <div><img style={{ height: '80px', width: '80px', marginLeft: '-14px' }} src={cart.image} /></div>
+class CartItem extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      quantity: 0
+    }
+  }
 
-        </div>
-        <div className='col-md-4'>
-          <div className='row' style={{ marginLeft: '-15px' }}>{cart.name}</div>
-          <div className='row' style={{ marginLeft: '-30px' }}>
-            <div className='col-md-3'><button>-  </button></div>
-            <div className='col-md-3' style={{ marginLeft: '10px'}}> {cart.qty} </div>
-            <div className='col-md-3'><button>+</button></div>
+ addQuantity = (cart) => {
+ cart.qty = cart.qty + 1
+ this.setState ({
+     cart : cart
+ })
+ }
+
+ reduceQuantity = (cart) => {
+     if (cart.qty > 1 ){
+    cart.qty = cart.qty - 1
+    this.setState ({
+        cart : cart
+    })
+}
+    }
+
+  render () {
+    const { cart } = this.props
+    const quantity = cart.qty
+    return (
+      <>
+        <div className='row'>
+          <div className='col-md-4'>
+            <div><img style={{ height: '80px', width: '80px', marginLeft: '-14px' }} src={cart.image} /></div>
 
           </div>
-        </div>
-        <div className='col-md-4'>
-       {cart.price}
-        </div>
-      </div>
+          <div className='col-md-4'>
+            <div className='row' style={{ marginLeft: '-15px' }}>{cart.name}</div>
+            <div className='row' style={{ marginLeft: '-30px' }}>
+              <div className='col-md-3'><button onClick={()=>(this.reduceQuantity(cart))}>-  </button></div>
+              <div className='col-md-3' style={{ marginLeft: '10px' }}> {cart.qty} </div>
+              <div className='col-md-3'><button onClick={()=>(this.addQuantity(cart))}>+</button></div>
 
-    </>
-  )
+            </div>
+          </div>
+          <div className='col-md-4'>
+            {cart.price}
+          </div>
+        </div>
+
+      </>
+    )
+  }
 }
-
 export default CartItem
